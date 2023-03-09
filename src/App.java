87b73743c0754dd646d69a681d1473a156960a79
Guilherme Ochoa <guilhermeochoa@gmail.com>
2023-03-09 00:42:49 +0000

    solicitaçoes 1,2,3

--- src/App.java
+++ src/App.java
@@ -2,9 +2,15 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
+        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45); 
+        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 55); 
+        Carro suv = new Carro("SUV", TipoCombustivel.GASOLINA, 8, 70);       
 
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
+        System.out.println(esportivo);
+        System.out.println(utilitario);
+
 
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
